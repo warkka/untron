a128322b3b16ec03ebfeb2194aa70fe32150da7f
jordan <41rjordan@proton.me>
2024-07-27 16:24:07 +0400

    import protobufs (does not build yet!!!)

--- script/build.rs
+++ script/build.rs
@@ -1,5 +1,34 @@
 use sp1_helper::build_program;
 
-fn main() {
-    build_program("../program")
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_program("../program");
+
+    copy_dir_all(
+        Path::new("googleapis/google"),
+        Path::new("java-tron/protocol/src/main/protos/google"),
+    )?;
+    tonic_build::configure().build_server(false).compile(
+        &["java-tron/protocol/src/main/protos/api/api.proto"],
+        &["java-tron/protocol/src/main/protos"],
+    )?;
+    fs::remove_dir_all("java-tron/protocol/src/main/protos/google")?;
+    Ok(())
 }
