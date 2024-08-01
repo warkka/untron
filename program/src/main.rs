2ef6f4c9085ff11cd9f67ea245f1afae47c30620
jordan <41rjordan@proton.me>
2024-08-01 15:13:51 +0400

    refactored two-step fee system

--- program/src/main.rs
+++ program/src/main.rs
@@ -19,12 +19,13 @@ use crypto::*;
 //
 // our napkin benchmark shows about 5k cycles per 1 tx and there's a little overhead
 // for special cases and kinda reward for the relayer
-const RECONSTRUCT_CYCLE_COUNT: u32 = 10000;
+const RECONSTRUCT_CYCLE_COUNT: u64 = 10000;
 
-const ORDER_TTL: u32 = 300_000; // milliseconds
+const ORDER_TTL: u32 = 100; // blocks
+const BLOCK_TIME: u32 = 3000; // milliseconds
 
 type PublicValues = sol! {
-    tuple(uint32,uint32,bytes32,bytes32,bytes32,bytes32,bytes32,uint64)
+    tuple(uint32,uint32,bytes32,bytes32,bytes32,bytes32,bytes32,bytes32,uint64)
 };
 
 type OrderChain = sol! {
@@ -35,15 +36,22 @@ type Block = sol! {
     tuple(bytes32,uint32,bytes32,uint32)
 };
 
-type State = sol! {
-    tuple(address,uint32,uint64,(address,uint64)[])[]
+type EncodedState = sol! {
+    tuple(address,uint64,(address,uint32,uint64,uint64)[])
 };
+
+#[derive(Debug)]
+struct State {
+    address: [u8; 20],
+    total_cost: u64,
+    orders: HashMap<[u8; 20], OrderState>,
+}
 
 #[derive(Clone, Debug)]
 struct OrderState {
     timestamp: u32,
     inflow: u64,
-    relayer_costs: HashMap<[u8; 20], u64>,
+    relay_cost: u64,
 }
 
 fn blockprint(prev_blockprint: &[u8; 32], blocks: &[(u32, [u8; 32], u32)]) -> [u8; 32] {
@@ -55,25 +63,15 @@ fn blockprint(prev_blockprint: &[u8; 32], blocks: &[(u32, [u8; 32], u32)]) -> [u
     blockprint
 }
 
-fn build_state_hash(state: &HashMap<[u8; 20], OrderState>) -> [u8; 32] {
+fn build_state_hash(state: &State) -> [u8; 32] {
     #[allow(clippy::type_complexity)]
-    let tupled_state: Vec<([u8; 20], u32, u64, Vec<([u8; 20], u64)>)> = state
+    let tupled_state: Vec<([u8; 20], u32, u64, u64)> = state
+        .orders
         .iter()
-        .map(|(address, order)| {
-            (
-                *address,
-                order.timestamp,
-                order.inflow,
-                order
-                    .relayer_costs
-                    .iter()
-                    .map(|(relayer, cost)| (*relayer, *cost))
-                    .collect::<Vec<([u8; 20], u64)>>(),
-            )
-        })
+        .map(|(address, order)| (*address, order.timestamp, order.inflow, order.relay_cost))
         .collect();
 
-    let state_print = State::abi_encode(&tupled_state);
+    let state_print = EncodedState::abi_encode(&(state.address, state.total_cost, tupled_state));
 
     let mut state_hash = [0u8; 32];
     state_hash.copy_from_slice(&hash(&state_print));
@@ -81,67 +79,76 @@ fn build_state_hash(state: &HashMap<[u8; 20], OrderState>) -> [u8; 32] {
 }
 
 pub fn main() {
-    let mut state = HashMap::new();
-    let mut closed_orders = HashMap::new();
-    let mut active_addresses = HashSet::new();
-
-    let my_address = read::<[u8; 20]>();
-
-    let state_length = read::<u32>();
-    for _ in 0..state_length {
-        let mut relayer_costs = HashMap::new();
-
+    let mut old_orders = HashMap::new();
+    let old_orders_length = read::<u32>();
+    for _ in 0..old_orders_length {
         let address = read::<[u8; 20]>();
         let timestamp = read::<u32>();
         let inflow = read::<u64>();
-        let relayers_count = read::<u32>();
+        let relay_cost = read::<u64>();
 
-        for _ in 0..relayers_count {
-            let relayer_address = read::<[u8; 20]>();
-            let relayer_cost = read::<u64>();
-            relayer_costs.insert(relayer_address, relayer_cost);
-        }
-
-        state.insert(
+        old_orders.insert(
             address,
             OrderState {
                 timestamp,
                 inflow,
-                relayer_costs,
+                relay_cost,
             },
         );
     }
 
-    let old_state_hash = build_state_hash(&state);
+    let old_state = State {
+        address: read::<[u8; 20]>(),
+        total_cost: read::<u64>(),
+        orders: old_orders,
+    };
 
-    let mut order_chain = read::<[u8; 32]>();
+    let old_state_hash = build_state_hash(&old_state);
+
+    let mut state = State {
+        address: read::<[u8; 20]>(),
+        total_cost: 0,
+        orders: old_state.orders,
+    };
+    let mut closed_orders = State {
+        address: old_state.address,
+        total_cost: old_state.total_cost,
+        orders: HashMap::new(),
+    };
+
+    let start_order_chain = read::<[u8; 32]>();
+    let mut end_order_chain = start_order_chain;
     let order_count = read::<u32>();
     for _ in 0..order_count {
         let mut element = Vec::with_capacity(32 * 3);
-        element.extend_from_slice(&order_chain);
+        element.extend_from_slice(&end_order_chain);
 
         let timestamp = read::<u32>();
         let address = read::<[u8; 20]>();
 
-        let order = OrderChain::abi_encode(&(order_chain, timestamp, address));
-        order_chain.copy_from_slice(&hash(&order));
+        let order = OrderChain::abi_encode(&(end_order_chain, timestamp, address));
+        end_order_chain.copy_from_slice(&hash(&order));
 
-        state.insert(
+        state.orders.insert(
             address,
             OrderState {
                 timestamp,
                 inflow: 0,
-                relayer_costs: HashMap::new(),
+                relay_cost: 0,
             },
         );
     }
 
     let start_block = read::<u32>();
     let end_block = read::<u32>();
-
     let mut end_blockprint = read::<[u8; 32]>(); // blockprint(start_block - 1)
-    let mcycles_cost = read::<u64>(); // USDT cost per 1M cycles
 
+    assert!(end_block - start_block + 1 > ORDER_TTL); // so that we don't have >1 queued relayers
+
+    let mcycles_cost = read::<u64>(); // USDT cost per 1M cycles
+    let cost_per_tx = RECONSTRUCT_CYCLE_COUNT * mcycles_cost; // WARNING: div by 1m is not applied
+
+    let mut active_addresses = HashSet::new();
     for block_number in start_block..=end_block {
         let timestamp = read::<u32>();
         let tx_count = read::<u32>();
@@ -153,11 +160,11 @@ pub fn main() {
             txs.push(read_vec());
         }
 
-        let state_copy = state.clone();
-        for (address, order) in state_copy {
-            if timestamp > order.timestamp + ORDER_TTL {
-                state.remove(&address);
-                closed_orders.insert(address, order);
+        let orders_copy = state.orders.clone();
+        for (address, order) in orders_copy {
+            if timestamp > order.timestamp + ORDER_TTL * BLOCK_TIME {
+                state.orders.remove(&address);
+                closed_orders.orders.insert(address, order);
                 active_addresses.remove(&address);
             } else if timestamp < order.timestamp {
                 continue;
@@ -168,9 +175,9 @@ pub fn main() {
 
         println!(
             "{} {} {}",
-            state.len(),
+            state.orders.len(),
             active_addresses.len(),
-            closed_orders.len()
+            closed_orders.orders.len()
         );
 
         // if active_addresses.is_empty() {
@@ -188,25 +195,22 @@ pub fn main() {
             };
 
             if active_addresses.contains(&transfer.to) {
-                state.get_mut(&transfer.to).unwrap().inflow += transfer.value;
+                state.orders.get_mut(&transfer.to).unwrap().inflow += transfer.value;
             }
         }
 
         let aa_count = active_addresses.len() as u64;
 
+        let cost_per_block = cost_per_tx * txs.len() as u64 / 1_000_000u64;
+        let cost_per_address = cost_per_block.div_ceil(aa_count);
+        state.total_cost += cost_per_address * aa_count;
+
         for address in active_addresses.iter() {
-            let order_state = state.get_mut(address).unwrap();
-            *order_state.relayer_costs.entry(my_address).or_insert(0) +=
-                RECONSTRUCT_CYCLE_COUNT as u64 * mcycles_cost / aa_count; // WARNING: there's no division by 1m (per mcycles) bc it has to be done in the end
+            let order_state = state.orders.get_mut(address).unwrap();
+            order_state.relay_cost += cost_per_address;
         }
 
         end_blockprint = blockprint(&end_blockprint, &[(block_number, tx_root, timestamp)]);
-    }
-
-    // if we did this on active_addresses.iter() it'd constantly lose precision
-    for (_, order_state) in state.iter_mut() {
-        let cost = order_state.relayer_costs.get_mut(&my_address).unwrap();
-        *cost = cost.div_ceil(1_000_000u64);
     }
 
     let new_state_hash = build_state_hash(&state);
@@ -218,7 +222,8 @@ pub fn main() {
         start_block,
         end_block,
         end_blockprint,
-        order_chain,
+        start_order_chain,
+        end_order_chain,
         old_state_hash,
         new_state_hash,
         closed_orders_hash,
