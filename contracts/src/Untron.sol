67c2cf3f14b048fd1f58c0d69766dbbc1fdd15ed
jordan <41rjordan@proton.me>
2024-08-03 20:48:11 +0400

    feat(contracts): reveal deposits

--- contracts/src/Untron.sol
+++ contracts/src/Untron.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ITronRelay {
-    function latestBlock() external returns (uint32);
-    function blockprint(uint32) external returns (bytes32);
+    function latestBlock() external returns (bytes32);
+    function blockExists(bytes32) external returns (bool);
+    function timestamp(bytes32) external returns (uint32);
 }
 
 interface ISender {
@@ -19,16 +20,17 @@ contract Untron is Ownable {
 
     constructor() Ownable(msg.sender) {}
 
+    uint64 constant ORDER_TTL = 100; // 100 blocks = 5 min
+
     struct Params {
         ISP1Verifier verifier;
         ITronRelay relay;
         ISender sender;
         bytes32 vkey;
-        uint64 mcyclesCost;
-        uint64 fulfillerFee;
+        uint64 feePerBlock;
         address relayer;
-        uint64 relayerFee;
         address paymaster;
+        uint64 revealerFee;
     }
 
     Params public params;
@@ -38,106 +40,107 @@ contract Untron is Ownable {
     }
 
     bytes32 public stateHash = bytes32(0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855); // sha256("")
-    uint32 public latestKnownBlock;
-    bytes32 public orderChain;
-    bytes32 public latestKnownOrderChain;
+    bytes32 public latestKnownBlock;
+    bytes32 public latestKnownOrder;
 
-    mapping(bytes32 => uint256) public orderIndex;
+    bytes32 public latestOrder;
+    uint256 public totalOrders;
+
+    mapping(bytes32 => uint32) public orderCreation; // order chained hash -> tron block
     mapping(address => Order) public orders; // tron address -> Order
     mapping(address => address) public tronAddresses; // tron address -> evm address
     mapping(address => Buyer) public buyers; // EVM address -> Buyer
     mapping(address => uint256) public orderCount; // tron address -> order count
 
     struct Order {
-        uint32 timestamp;
+        uint64 size;
+        uint64 rate;
         ISender sender;
-        uint64 amount;
         bytes transferData;
         address fulfiller;
         uint64 fulfilledAmount;
-        uint64 relayerFee;
+        uint64 fulfillerFee;
     }
 
     struct Buyer {
         address _address;
         uint64 liquidity;
         uint64 order;
-        uint64 minOrderSize;
-    }
-
-    struct State {
-        address relayer;
-        uint64 totalCost;
-        OrderState[] orders;
+        uint64 minDeposit;
     }
 
     struct OrderState {
         address tronAddress;
         uint32 timestamp;
         uint64 inflow;
-        uint64 relayCost;
+        uint64 minDeposit;
     }
 
-    function revealDeposits(bytes calldata proof, bytes calldata publicValues, State calldata closedOrders) external {
+    function revealDeposits(bytes calldata proof, bytes calldata publicValues, OrderState[] calldata closedOrders)
+        external
+    {
         require(msg.sender == params.relayer || params.relayer == address(0));
 
         params.verifier.verifyProof(params.vkey, publicValues, proof);
         (
-            uint32 startBlock,
-            uint32 endBlock,
-            bytes32 endBlockprint,
-            bytes32 startOrderChain,
-            bytes32 endOrderChain,
+            address relayer,
+            bytes32 startBlock,
+            bytes32 endBlock,
+            bytes32 startOrder,
+            bytes32 endOrder,
             bytes32 oldStateHash,
             bytes32 newStateHash,
             bytes32 closedOrdersHash,
-            uint64 _mcyclesCost
-        ) = abi.decode(publicValues, (uint32, uint32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, uint64));
+            uint64 _feePerBlock,
+            uint64 totalFee
+        ) = abi.decode(
+            publicValues, (address, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, uint64, uint64)
+        );
 
-        require(startBlock <= latestKnownBlock);
-        require(endBlock <= params.relay.latestBlock() - 19);
-        require(endBlockprint == params.relay.blockprint(endBlock));
-        require(orderIndex[startOrderChain] <= orderIndex[orderChain]);
-        require(orderIndex[endOrderChain] > orderIndex[latestKnownOrderChain]);
+        require(msg.sender == relayer);
+        require(startBlock == latestKnownBlock);
+        require(params.relay.blockExists(endBlock));
+        require(startOrder == latestKnownOrder);
+        require(orderCreation[endOrder] > totalOrders);
         require(oldStateHash == stateHash);
-        require(_mcyclesCost <= params.mcyclesCost);
+        require(_feePerBlock == params.feePerBlock);
 
         stateHash = newStateHash;
-        latestKnownOrderChain = endOrderChain;
+        latestKnownOrder = endOrder;
 
         require(sha256(abi.encode(closedOrders)) == closedOrdersHash);
 
-        uint256 paymasterFine = 0;
-        for (uint256 i = 0; i < closedOrders.orders.length; i++) {
-            OrderState memory orderState = closedOrders.orders[i];
-            Order memory order = orders[orderState.tronAddress];
+        uint64 paymasterFine = 0;
+        for (uint256 i = 0; i < closedOrders.length; i++) {
+            OrderState memory state = closedOrders[i];
+            Order memory order = orders[state.tronAddress];
 
-            uint64 amount = orderState.inflow;
-            uint64 costOverdraft = orderState.relayCost - amount;
-            amount -= orderState.relayCost;
-            amount -= order.relayerFee;
-            amount = amount > order.amount ? order.amount : amount;
-            uint64 surplus = order.amount - amount;
+            uint64 amount = order.size < state.inflow ? order.size : state.inflow;
+            amount = amount * 1e6 / order.rate;
+            uint64 _paymasterFine = params.feePerBlock * ORDER_TTL - amount;
+            amount -= params.feePerBlock * ORDER_TTL;
+            amount -= params.revealerFee;
+            uint64 left = order.size - amount;
+            buyers[tronAddresses[state.tronAddress]].liquidity += left;
 
-            if (costOverdraft != 0) {
-                paymasterFine += costOverdraft;
-            } else if (order.fulfilledAmount == amount) {
+            paymasterFine += _paymasterFine;
+            if (order.fulfilledAmount == amount) {
                 require(usdt.transfer(order.fulfiller, amount));
             } else {
                 order.sender.send(amount, order.transferData);
             }
-
-            buyers[tronAddresses[orderState.tronAddress]].liquidity += surplus;
-            delete orders[orderState.tronAddress];
         }
+        totalFee += params.revealerFee * uint64(closedOrders.length);
 
-        // no "require"
-        usdt.transferFrom(params.paymaster, address(this), paymasterFine);
-        usdt.transfer(closedOrders.relayer, closedOrders.totalCost);
+        require(usdt.transfer(msg.sender, totalFee));
+    }
+
+    function blockIdToNumber(bytes32 blockId) internal pure returns (uint256) {
+        return uint256(blockId) >> 192;
     }
 
     function jailbreak() external {
-        require(params.relay.latestBlock() > latestKnownBlock + 300); // 15 minutes
+        require(blockIdToNumber(params.relay.latestBlock()) > blockIdToNumber(latestKnownBlock) + 600); // 30 minutes
         params.relayer = address(0);
     }
 }
