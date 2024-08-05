c944ede7bd9fb5aa2877a9db64db972a497548e5
jordan <41rjordan@proton.me>
2024-08-05 19:38:27 +0400

    modify zktron for Untron SC compatibility

--- circuits/zktron/src/main.rs
+++ circuits/zktron/src/main.rs
@@ -8,7 +8,15 @@ use sp1_zkvm::io::*;
 use zktron::*;
 
 type LightClientOutput = sol! {
-    tuple(bytes32,bytes32,uint32,bytes32,bytes32)
+    tuple(bytes32,bytes32,bytes32)
+};
+
+type ByteArray = sol! {
+    bytes32[]
+};
+
+type BytesArray = sol! {
+    bytes[]
 };
 
 pub fn main() {
@@ -16,16 +24,16 @@ pub fn main() {
     let mut end_block = start_block;
 
     let mut srs = HashSet::new();
-    let mut sr_print = Vec::with_capacity(64 * 27);
+    let mut sr_print = Vec::with_capacity(27);
     for _ in 0..27 {
         let sr = read_vec();
-        sr_print.extend_from_slice(&sr);
+        sr_print.push(sr.clone());
         srs.insert(sr);
     }
-    let sr_print = hash(&sr_print);
+    let sr_print = hash(&BytesArray::abi_encode(&sr_print));
 
     let block_count = read::<u32>();
-    let mut block_ids = Vec::with_capacity((32 * block_count) as usize);
+    let mut block_ids = Vec::with_capacity(block_count as usize);
     let mut cycle = Vec::with_capacity(18);
 
     for _ in 0..block_count {
@@ -47,12 +55,11 @@ pub fn main() {
         assert_eq!(block_header.prev_block_id, end_block);
         end_block = block_header.new_block_id;
 
-        block_ids.extend_from_slice(&block_header.new_block_id);
+        block_ids.push(block_header.new_block_id);
     }
 
-    let blockprint = hash(&block_ids);
-    let output =
-        LightClientOutput::abi_encode(&(start_block, end_block, block_count, sr_print, blockprint));
+    let blockprint = hash(&ByteArray::abi_encode(&block_ids));
+    let output = LightClientOutput::abi_encode(&(start_block, sr_print, blockprint));
 
     commit_slice(&output);
 }
