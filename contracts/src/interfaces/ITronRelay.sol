627e50d44779848a2e8715f31ce2bdedc47ed415
jordan <41rjordan@proton.me>
2024-08-10 10:33:46 +0400

    fix ITronRelay interface

--- contracts/src/interfaces/ITronRelay.sol
+++ contracts/src/interfaces/ITronRelay.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ITronRelay {
-    function latestBlock() external returns (uint256);
+    function latestBlockNumber() external returns (uint256);
     function blocks(uint256) external returns (bytes32);
 }
