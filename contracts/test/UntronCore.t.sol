206dd822c8c93e3c38f54bd3f296bd2a661f603f
jordan <41rjordan@proton.me>
2024-09-28 21:46:13 +0500

    fix(MockUSDT): 18 -> 6 decimals

--- contracts/test/UntronCore.t.sol
+++ contracts/test/UntronCore.t.sol
@@ -15,6 +15,10 @@ contract MockUSDT is ERC20 {
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
 }
 
 contract UntronCoreTest is Test {
